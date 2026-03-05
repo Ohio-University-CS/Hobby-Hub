0a3e4b4636b6efc5eedb38c322e4c999c64a4e11
Collin Blumenauer <92476364+CollinInMySternum@users.noreply.github.com>
2026-03-05 13:03:50 -0500

    Generated Prisma Client fixes

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,7 +1,9 @@
-import { PrismaClient } from "@generated/prisma";
+import { PrismaClient as GeneratedPrismaClient } from "@/generated/prisma/client";
 
-const globalForPrisma = global as unknown as { prisma: PrismaClient };
+const globalForPrisma = global as unknown as { prisma?: GeneratedPrismaClient };
 
-export const prisma = globalForPrisma.prisma || new PrismaClient();
+export const prisma =
+  globalForPrisma.prisma ??
+  new GeneratedPrismaClient({} as ConstructorParameters<typeof GeneratedPrismaClient>[0])
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
