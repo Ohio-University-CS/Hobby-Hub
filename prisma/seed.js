7c26003ca31ef04d4f7c95caeff61a96553b25c0
Collin Blumenauer <92476364+CollinInMySternum@users.noreply.github.com>
2026-03-30 22:11:43 -0400

    Better Interests

--- prisma/seed.js
+++ prisma/seed.js
@@ -6,22 +6,47 @@ const connectionString = `${process.env.DATABASE_URL}`;
 const adapter = new PrismaPg({ connectionString });
 const prisma = new PrismaClient({ adapter });
 
-async function main() {
-    const interests = [
-        "react",
-        "nextjs",
-        "typescript",
-        "javascript",
-        "nodejs",
-        "docker",
-        "ai",
-        "design",
-        "web",
-        "webdev",
-        "game-dev",
-        "game-development",
-    ];
+const interests = [
+    "Reading",
+    "Writing",
+    "Drawing",
+    "Painting",
+    "Photography",
+    "Music",
+    "Singing",
+    "Dancing",
+    "Cooking",
+    "Coding",
+    "Baking",
+    "Electronics",
+    "Gardening",
+    "Hiking",
+    "Walking",
+    "Running",
+    "Cycling",
+    "Swimming",
+    "Traveling",
+    "Fitness",
+    "Yoga",
+    "Meditation",
+    "Watching Movies/TV",
+    "Learning Languages",
+    "Volunteering",
+    "Journaling",
+    "Crafting",
+    "DIY Projects",
+    "Bird Watching",
+    "Fishing",
+    "Camping",
+    "Socializing",
+    "Collecting",
+    "Board Games",
+    "Puzzles",
+    "Sports",
+    "Video Games"
+];
 
+async function main() {
     for(const name of interests) {
         await prisma.interest.upsert({
             where: {name},
