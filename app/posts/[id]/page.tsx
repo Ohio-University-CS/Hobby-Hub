2d336bf4c54cf0601d45152cfd46a9dbecb6ae2f
Collin Blumenauer <92476364+CollinInMySternum@users.noreply.github.com>
2026-04-29 16:30:10 -0400

    Post MetaData support.

--- app/posts/[id]/page.tsx
+++ app/posts/[id]/page.tsx
@@ -1,4 +1,46 @@
 import { ViewPostPage } from "@/components/post-page";
+import { prisma } from "@/lib/prisma";
+
+import {remark} from 'remark';
+import strip from 'strip-markdown';
+
+type Props = {
+  params: Promise<{ id: string }>
+  searchParams: Promise<{ [key: string]: string | string[] | undefined }>
+}
+
+export async function generateMetadata({ params }: Props) {
+    const {id} = await params;
+    const postId = id as string;
+
+    
+    const post = await prisma.post.findUnique({
+        where: { id: postId },
+        select: {
+            title: true,
+            content: true,
+            media: true,
+        }
+    });
+    
+    if (!post) return {
+        title: "Post Not Found | HobbyHub",
+    }
+    const text = await remark().use(strip).process(post.content.substring(0, 157));
+
+    if(post.media.length > 0) {
+        return {
+            title: `${post.title} | HobbyHub`,
+            description: `${text}...`,
+            openGraph: { images: [post.media[0]] }
+        };
+    }
+
+    return {
+        title: `${post.title} | HobbyHub`,
+        description: `${text}...`,
+    }
+}
 
 const PostPage = () => {
     return (
